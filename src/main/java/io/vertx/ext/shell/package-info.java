6fb737a0140ac6400b81df0d17acf445d0a97dab
Julien Viet <julien@julienviet.com>
2015-06-17 15:49:02 +0200

    Initial import

---
+++ src/main/java/io/vertx/ext/shell/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Todo
+ */
+@GenModule(name = "vertx-shell")
+@Document(fileName = "index.adoc")
+package io.vertx.ext.shell;
+
+import io.vertx.codegen.annotations.GenModule;
+import io.vertx.docgen.Document;
