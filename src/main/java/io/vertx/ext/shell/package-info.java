7c3559fc112193ff0308a560c7ca8aa2de5e8a94
Julien Viet <julien@julienviet.com>
2015-09-03 16:44:53 +0200

    Doc embryo

--- src/main/java/io/vertx/ext/shell/package-info.java
+++ src/main/java/io/vertx/ext/shell/package-info.java
@@ -1,5 +1,29 @@
 /**
- * Todo
+ * = Vert.x Shell
+ *
+ * Vert.x Shell is a command line interface for the Vert.x runtime available from regular
+ * terminals using different protocols.
+ *
+ * Vert.x Shell provides a variety of commands for interacting live with Vert.x services.
+ *
+ * Vert.x Shell can be extended with custom commands in the language of your choice.
+ *
+ * == Using Vert.x Shell
+ *
+ * todo.
+ *
+ * == Shell Service
+ * 
+ * todo.
+ *
+ * == Base commands
+ *
+ * todo.
+ *
+ * == Extending Vert.x Shell
+ *
+ * todo.
+ *
  */
 @GenModule(name = "vertx-shell")
 @Document(fileName = "index.adoc")
