84d19d66b2873ab5535db0a7f6ecd1246a3155c7
Julien Viet <julien@julienviet.com>
2015-09-16 16:18:51 +0200

    Ls improvements

--- src/main/java/io/vertx/ext/shell/command/BaseCommands.java
+++ src/main/java/io/vertx/ext/shell/command/BaseCommands.java
@@ -6,6 +6,7 @@ import io.vertx.core.cli.Argument;
 import io.vertx.core.cli.CLI;
 import io.vertx.core.cli.Option;
 import io.vertx.core.eventbus.MessageConsumer;
+import io.vertx.core.file.FileProps;
 import io.vertx.core.http.impl.HttpServerImpl;
 import io.vertx.core.impl.Deployment;
 import io.vertx.core.impl.VertxInternal;
@@ -20,10 +21,18 @@ import io.vertx.ext.shell.registry.CommandRegistry;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.AbstractMap;
+import java.util.Date;
+import java.util.Formatter;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
@@ -254,20 +263,64 @@ public interface BaseCommands {
   }
 
   static Command fs_ls() {
-    Command cmd = Command.command("ls");
+    Option ELL = new Option().setArgName("ell").setFlag(true).setShortName("l").setDescription("List in long format");
+    Argument FILE = new Argument().setArgName("dir").setRequired(false).setDescription("The dir to list");
+    Option HELP = new Option().setArgName("help").setFlag(true).setShortName("h").setLongName("help").setDescription("This help");
+    Option ALL = new Option().setArgName("all").setFlag(true).setShortName("a").setDescription("Include files that begins with .");
+    Command cmd = Command.command(CLI.create("ls").
+        addOption(HELP).
+        addOption(ELL).
+        addOption(ALL).
+        addArgument(FILE).
+        setDescription("List directory content"));
     cmd.completionHandler(new FsHelper().completionHandler());
     cmd.processHandler(process -> {
+      String fileArg = process.commandLine().getArgumentValue(0);
       new FsHelper().ls(process.vertx(),
           process.session().get("path"),
-          process.args().size() > 0 ? process.args().get(0) : ".",
+          fileArg != null ? fileArg : ".",
           ar -> {
             if (ar.succeeded()) {
-              ar.result().forEach((file, props) -> {
-                String name = file.substring(file.lastIndexOf('/') + 1);
-                process.write(name + "\n");
-              });
-            } else {
-              process.write("ls:" + ar.cause().getMessage() + "\n");
+              Map<String, FileProps> result = ar.result();
+              if (result.size() > 0) {
+                Stream<Map.Entry<String, FileProps>> entries = result.entrySet().stream();
+
+                // Keep only name
+                entries = entries.map(entry -> new AbstractMap.SimpleEntry<>(entry.getKey().substring(entry.getKey().lastIndexOf('/') + 1), entry.getValue()));
+
+                // Filter -a option
+                if (!process.commandLine().isSeenInCommandLine(ALL)) {
+                  entries = entries.filter(entry -> !entry.getKey().startsWith("."));
+                }
+
+                // Format name
+                Function<Map.Entry<String, FileProps>, String> formatter;
+                if (process.commandLine().isSeenInCommandLine(ELL)) {
+                  int width = result.values().stream().map(FileProps::size).max(Long::compare).get().toString().length();
+                  String format = "%1$s %2$" + width + "s %3$tb %3$2te %3$tH:%3$tM %4$s";
+                  formatter = entry -> {
+                    FileProps props = entry.getValue();
+                    String a;
+                    if (props.isDirectory()) {
+                      a = "d";
+                    } else if (props.isSymbolicLink()) {
+                      a = "l";
+                    } else {
+                      a = "-";
+                    }
+                    return String.format(format, a, props.size(), new Date(props.lastModifiedTime()), entry.getKey());
+                  };
+                } else {
+                  formatter = Map.Entry::getKey;
+                }
+
+                // Here we go
+                entries.map(formatter).forEach(file -> {
+                  process.write(file + "\n");
+                });
+              } else {
+                process.write("ls:" + ar.cause().getMessage() + "\n");
+              }
             }
             process.end();
           });
