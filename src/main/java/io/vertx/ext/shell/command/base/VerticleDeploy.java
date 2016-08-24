edacfb04a153d2cf4a101318cf8013d0199e8934
Emad Alblueshi <emad.albloushi@gmail.com>
2016-08-24 18:40:48 +0300

    add deployment options as JSON string in verticle-deploy command

--- src/main/java/io/vertx/ext/shell/command/base/VerticleDeploy.java
+++ src/main/java/io/vertx/ext/shell/command/base/VerticleDeploy.java
@@ -32,10 +32,12 @@
 
 package io.vertx.ext.shell.command.base;
 
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.cli.annotations.Argument;
 import io.vertx.core.cli.annotations.Description;
 import io.vertx.core.cli.annotations.Name;
 import io.vertx.core.cli.annotations.Summary;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.shell.command.AnnotatedCommand;
 import io.vertx.ext.shell.command.CommandProcess;
 
@@ -51,24 +53,45 @@ public class VerticleDeploy extends AnnotatedCommand {
 
   private String name;
 
+  private String options;
+
   @Argument(index = 0, argName = "name")
   @Description("the verticle name")
   public void setName(String name) {
     this.name = name;
   }
 
+  @Argument(index = 1, argName = "options", required = false)
+  @Description("the verticle deployment options as JSON object string")
+  public void setOptions(String options) {
+    this.options = options;
+  }
   @Override
   public void process(CommandProcess process) {
-    process.vertx().deployVerticle(name, ar -> {
-      if (ar.succeeded()) {
-        process.write("Deployed " + ar.result() + "\n").end();
+    try {
+      DeploymentOptions deploymentOptions;
+      if(options != null && !options.isEmpty()) {
+        deploymentOptions = new DeploymentOptions(new JsonObject(options));
       } else {
-        process.write("Could not deploy " + name + "\n");
-        StringWriter buffer = new StringWriter();
-        PrintWriter writer = new PrintWriter(buffer);
-        ar.cause().printStackTrace(writer);
-        process.write(buffer.toString()).end();
+        deploymentOptions = new DeploymentOptions();
       }
-    });
+      process.vertx().deployVerticle(name, deploymentOptions, ar -> {
+        if (ar.succeeded()) {
+          process.write("Deployed " + ar.result() + "\n").end();
+        } else {
+          process.write("Could not deploy " + name + "\n");
+          StringWriter buffer = new StringWriter();
+          PrintWriter writer = new PrintWriter(buffer);
+          ar.cause().printStackTrace(writer);
+          process.write(buffer.toString()).end();
+        }
+      });
+    } catch (RuntimeException e) {
+      process.write("Invalid JSON object as a deployment options for " + name + "\n");
+      StringWriter buffer = new StringWriter();
+      PrintWriter writer = new PrintWriter(buffer);
+      e.printStackTrace(writer);
+      process.write(buffer.toString()).end();
+    }
   }
 }
