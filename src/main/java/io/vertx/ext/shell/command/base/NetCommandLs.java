8e993bf56864b3ba09f5c853b7ea6f886864092f
Julien Viet <julien@julienviet.com>
2024-05-07 16:24:11 +0200

    Update to vertx-core changes

--- src/main/java/io/vertx/ext/shell/command/base/NetCommandLs.java
+++ src/main/java/io/vertx/ext/shell/command/base/NetCommandLs.java
@@ -34,9 +34,8 @@ package io.vertx.ext.shell.command.base;
 
 import io.vertx.core.cli.annotations.Name;
 import io.vertx.core.cli.annotations.Summary;
-import io.vertx.core.http.impl.HttpServerImpl;
 import io.vertx.core.impl.VertxInternal;
-import io.vertx.core.net.impl.NetServerImpl;
+import io.vertx.core.net.impl.NetServerInternal;
 import io.vertx.core.net.impl.ServerID;
 import io.vertx.ext.shell.command.AnnotatedCommand;
 import io.vertx.ext.shell.command.CommandProcess;
@@ -47,18 +46,14 @@ import java.util.Map;
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
 @Name("net-ls")
-@Summary("List all servers")
+@Summary("List all TCP servers")
 public class NetCommandLs extends AnnotatedCommand {
 
   @Override
   public void process(CommandProcess process) {
     VertxInternal vertx = (VertxInternal) process.vertx();
-    process.write("\nNet Servers:\n");
-    for (Map.Entry<ServerID, NetServerImpl> server : vertx.sharedNetServers().entrySet()) {
-      process.write(server.getKey().host + ":" + server.getKey().port + "\n");
-    }
-    process.write("\nHTTP Servers:\n");
-    for (Map.Entry<ServerID, HttpServerImpl> server : vertx.sharedHttpServers().entrySet()) {
+    process.write("\nTCP Servers:\n");
+    for (Map.Entry<ServerID, NetServerInternal> server : vertx.sharedTcpServers().entrySet()) {
       process.write(server.getKey().host + ":" + server.getKey().port + "\n");
     }
     process.end();
